f3eeb5cebd0d3c0cc22814ba45a894ece73327ca
Hinter <schokkerino@gmail.com>
2020-03-16 20:05:46 +0100

    trash

--- AutoScouter/autoscouter.gradle.kts
+++ AutoScouter/autoscouter.gradle.kts
@@ -52,7 +52,7 @@ tasks {
                     "Plugin-Version" to project.version,
                     "Plugin-Id" to nameToId(project.extra["PluginName"] as String),
                     "Plugin-Provider" to project.extra["PluginProvider"],
-                    "Plugin-Dependencies" to nameToId("extutils"),
+                    "Plugin-Dependencies" to nameToId("extutilscustom"),
                     "Plugin-Description" to project.extra["PluginDescription"],
                     "Plugin-License" to project.extra["PluginLicense"]
             ))
